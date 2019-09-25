d53ce0f94c4d4c4d719f4e480531e933c59be570
Connor Imes <cimes@isi.edu>
2019-09-25 13:03:35 -0400

    run-app-numactl: add -l option

--- run-app-numactl.sh
+++ run-app-numactl.sh
@@ -9,15 +9,16 @@ function usage() {
     echo "Run an app using numactl"
     echo "Writes to stdout.log and stderr.log"
     echo ""
-    echo "Usage: $0 -a SH -t N -C CPUS [-h]"
+    echo "Usage: $0 -a SH -t N -C CPUS [-l FILE] [-h]"
     echo "    -a SH: bash script to source with app launch vars"
     echo "    -t N: number of threads for the app"
     echo "    -C CPUS: numactl physcpubind"
+    echo "    -l FILE: log file for start/end timing metrics"
     echo "    -h: print help/usage and exit"
     exit "$rc"
 }
 
-while getopts "a:t:C:h?" o; do
+while getopts "a:t:C:l:h?" o; do
     case "$o" in
         a)
             APP_SCRIPT=$OPTARG
@@ -27,6 +28,9 @@ while getopts "a:t:C:h?" o; do
             ;;
         C)
             CPUS=$OPTARG
+            ;;
+        l)
+            LOG=$OPTARG
             ;;
         h)
             usage
@@ -42,6 +46,9 @@ if [ -z "$APP_SCRIPT" ] || [ ! -f "$APP_SCRIPT" ] ||
    [ -z "$CPUS" ] || [ -z "$NTHREADS" ]; then
     usage 1
 fi
+if [ -z "$LOG" ]; then
+    LOG=/dev/null
+fi
 
 source "$APP_SCRIPT" || exit 1
 if [ ${#APP_CMD[@]} -eq 0 ]; then
@@ -54,11 +61,11 @@ fi
 
 echo "$APP_NAME: app_pre"
 app_pre "$NTHREADS" || exit $?
-echo "$APP_NAME: start: $(datetime)"
+echo "$APP_NAME: start: $(datetime)" | tee "$LOG"
 echo "$APP_NAME: run: numactl -l -C $CPUS -- ${APP_CMD[*]}"
 numactl -l -C "$CPUS" "${APP_CMD[@]}" > >(tee stdout.log) 2> >(tee stderr.log)
 rc=$?
-echo "$APP_NAME: end: $(datetime)"
+echo "$APP_NAME: end: $(datetime)" | tee -a "$LOG"
 echo "$APP_NAME: app_post"
 app_post || exit $?
 exit $rc
