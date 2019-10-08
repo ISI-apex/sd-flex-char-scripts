5889306d71422e12f0bb69a2b352c2b86e03e588
Connor Imes <cimes@isi.edu>
2019-10-08 11:35:03 -0400

    env-reference: add STREAM to PATH

--- env-reference.sh
+++ env-reference.sh
@@ -3,7 +3,10 @@
 # A default system configuration
 #
 
-# Configure LD_LIBRARY_PATH, e.g., for libgfortran
+# Configure PATH for tools, e.g., for OpenMPI installation
+export PATH=$PATH
+
+# Configure LD_LIBRARY_PATH for tools, e.g., for libgfortran
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
 # Configure NAS Parallel Benchmarks binary location
@@ -12,3 +15,7 @@ NPB_DIR=${HOME}/NPB${NPB_VER}
 NPB_OMP_DIR=${NPB_DIR}/NPB${NPB_VER}-OMP
 NPB_MPI_DIR=${NPB_DIR}/NPB${NPB_VER}-MPI
 export PATH=${NPB_OMP_DIR}/bin:${NPB_MPI_DIR}/bin:$PATH
+
+# Configure STREAM location
+STREAM_DIR=${HOME}/stream
+export PATH=${STREAM_DIR}:$PATH
