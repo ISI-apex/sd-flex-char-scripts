0095baa850d693e4a60e107cfa9b19fd88881aed
Connor Imes <cimes@isi.edu>
2020-03-30 10:20:39 -0400

    topology: rm unused topology_sock_to_taskset function
    It probably isn't quite doing what we'd want anyway b/c of HyperThreads

--- topology.sh
+++ topology.sh
@@ -35,32 +35,6 @@ function topology_dbg() {
     echo "TOPOLOGY_SOCKET_CPUS: $TOPOLOGY_SOCKET_CPUS"
 }
 
-# Get a bitbask for $1 cpus
-function topology_cpus_to_bitmask() {
-    local n=$1
-    local mask=0
-    for ((i = 0; i < n; i++)); do
-        mask=$((mask << 1 | 1))
-    done
-    printf "%x\n" $mask
-}
-
-# The taskset for socket $1, for $2 cpus, at optional offset $3
-# Does not verify that cpus or offset remain within socket!
-function topology_sock_to_taskset() {
-    # assumes CPU numbers are ordered globally by all physical, then all virtual
-    # (not interleaved at socket/core granularity, which is also a valid scheme)
-    local sock=$1
-    local cpus=$2
-    local off=$3
-    [ -z "$off" ] && off=0
-    local ts
-    ts=$(topology_cpus_to_bitmask "$cpus")
-    ts=$((ts << (sock * TOPOLOGY_SOCKET_CORES)))
-    ts=$((ts << off))
-    printf "%x\n" "$ts"
-}
-
 # Get CPU range for socket $1, for $2 cpus, at optional offset $3
 # Does not verify that cpus or offset remain within socket!
 function topology_sock_to_physcpubind() {
