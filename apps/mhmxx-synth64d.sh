8201762484e2557db3485cfcce9311b1624c416a
Connor Imes <cimes@isi.edu>
2020-03-31 15:34:59 -0400

    apps: add mhmxx configs

---
+++ apps/mhmxx-synth64d.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export APP_NAME="mhmxx-synth64d"
+APP_BIN=$(which mhmxx) # upcxx-run seems to need the full path
+DATASET=/dev/shm/synth64d.fastq
+export APP_CMD=()
+
+function app_pre() {
+    # NOTE: cores-per-node is platform-specific!
+    APP_CMD+=(upcxx-run -shared-heap "5%" -n "$1" --
+              "$APP_BIN" --cores-per-node 24 -r "$DATASET" -i 300:30 -k 21,33,55,77,99 -s 99,33)
+}
+
+function app_post() {
+    rm -f *.fast*
+}
