7894f0817b91e87c101b6ee3afb664230c2e549b
Connor Imes <cimes@isi.edu>
2020-04-21 09:11:17 -0400

    apps: add mhmxx-c2d2624-* app versions

---
+++ apps/mhmxx-c2d2624-gut5x.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export APP_NAME="mhmxx-gut5x"
+APP_BIN=$(which mhmxx) # upcxx-run seems to need the full path
+DATASET=/dev/shm/gut-5x.fastq
+export APP_CMD=()
+
+function app_pre() {
+    # NOTE: cores-per-node is platform-specific!
+    if [ -z "$MHMXX_CORES_PER_NODE" ]; then
+        >&2 echo "ERROR: Must set env var MHMXX_CORES_PER_NODE (e.g., depending on HyperThreads)"
+        return 1
+    fi
+    APP_CMD+=(upcxx-run -shared-heap "5%" -n "$1" --
+              "$APP_BIN" --cores-per-node "$MHMXX_CORES_PER_NODE" -r "$DATASET" -i "300:30" -k "21,33,55,77,99" -s "99,33")
+}
+
+function app_post() {
+    rm -f ./*.fast*
+}
