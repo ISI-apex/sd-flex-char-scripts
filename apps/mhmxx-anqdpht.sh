bb03aac110a4186e0581840b80e7bbd6bf01e4c7
Connor Imes <cimes@isi.edu>
2020-04-21 09:12:34 -0400

    apps: mhmxx: update to latest app HEAD version (c2d2624)

--- apps/mhmxx-anqdpht.sh
+++ apps/mhmxx-anqdpht.sh
@@ -5,15 +5,10 @@ DATASET=/dev/shm/7537.7.76903.ACTTGA.anqdpht.fastq
 export APP_CMD=()
 
 function app_pre() {
-    # NOTE: cores-per-node is platform-specific!
-    if [ -z "$MHMXX_CORES_PER_NODE" ]; then
-        >&2 echo "ERROR: Must set env var MHMXX_CORES_PER_NODE (e.g., depending on HyperThreads)"
-        return 1
-    fi
     APP_CMD+=(upcxx-run -shared-heap "5%" -n "$1" --
-              "$APP_BIN" --cores-per-node "$MHMXX_CORES_PER_NODE" -r "$DATASET" -i "300:30" -k "21,33,55,77,99" -s "99,33")
+              "$APP_BIN" -r "$DATASET" -o "mhmxx_out" -i "300:30" -k "21,33,55,77,99" -s "99,33")
 }
 
 function app_post() {
-    rm -f ./*.fast*
+    rm -f mhmxx_out/*.fast*
 }
