e62898da1e2cea76a499ccb0c2b5693077679911
Connor Imes <cimes@isi.edu>
2020-06-09 15:05:27 -0400

    run-app-openmpi: add param for interceptor

--- run-app-openmpi.sh
+++ run-app-openmpi.sh
@@ -8,8 +8,9 @@ function usage() {
     echo "Run an app using OpenMPI"
     echo "Writes to '1/rank.{0..\$N-1}/std{out,err}'"
     echo ""
-    echo "Usage: $0 -a SH [-n N] [-t N] [-m FOO] [-b FOO] [-r FILE] [-l FILE] [-h] -- [MPI_OPTION]..."
+    echo "Usage: $0 -a SH [-i SH] [-n N] [-t N] [-m FOO] [-b FOO] [-r FILE] [-l FILE] [-h] -- [MPI_OPTION]..."
     echo "    -a SH: bash script to source with app launch vars"
+    echo "    -i SH: bash script to source with interceptor configurations"
     echo "    -n N: number of MPI processes; see OpenMPI -np (default=1)"
     echo "    -t N: number of threads for each process; see OpenMPI PE=n (default=1)"
     echo "    -m FOO: map processes by FOO; see OpenMPI --map-by (default=core)"
@@ -26,10 +27,13 @@ NTHREADS=1
 MAP_BY=core
 BIND_TO=core
 RANK_FILE=
-while getopts "a:n:t:m:b:r:l:h?" o; do
+while getopts "a:i:n:t:m:b:r:l:h?" o; do
     case "$o" in
         a)
             APP_SCRIPT=$OPTARG
+            ;;
+        i)
+            INTERCEPTOR_SCRIPT=$OPTARG
             ;;
         n)
             NPROCS=$OPTARG
@@ -73,6 +77,13 @@ if [ -z "$APP_NAME" ]; then
     APP_NAME="<UNKNOWN>"
 fi
 
+if [ -n "$INTERCEPTOR_SCRIPT" ]; then
+    source "$INTERCEPTOR_SCRIPT" || exit $?
+    if [ -z "$INTERCEPTOR_NAME" ]; then
+        INTERCEPTOR_NAME="<UNKNOWN>"
+    fi
+fi
+
 RUN_CMD=(mpirun)
 if [ -n "$RANK_FILE" ]; then
     RUN_CMD+=(-rf "$RANK_FILE")
@@ -85,12 +96,22 @@ RUN_CMD+=(--report-bindings --output-filename . "$@" --)
 
 echo "$APP_NAME: app_pre"
 app_pre "$NPROCS" "$NTHREADS" || exit $?
+if [ -n "$INTERCEPTOR_SCRIPT" ]; then
+    trap trap_interceptor EXIT
+    echo "$INTERCEPTOR_NAME: interceptor_start"
+    interceptor_start || exit $?
+fi
 RUN_CMD+=("${APP_CMD[@]}")
 echo "$APP_NAME: start: $(datetime)" | tee "$LOG"
 echo "$APP_NAME: run: ${RUN_CMD[*]}" | tee -a "$LOG"
 "${RUN_CMD[@]}"
 rc=$?
 echo "$APP_NAME: end: $(datetime)" | tee -a "$LOG"
+if [ -n "$INTERCEPTOR_SCRIPT" ]; then
+    echo "$INTERCEPTOR_NAME: interceptor_stop"
+    interceptor_stop || rc=$?
+    trap - EXIT
+fi
 echo "$APP_NAME: app_post"
 app_post || exit $?
 exit $rc
